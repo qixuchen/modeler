cc728882ce0b6b899a3bcc98dad26d5912a21d2d
Qixu Chen <qchenax@uw.edu>
2019-03-14 19:34:21 +0800

    添加项目文件。

---
+++ sample.cpp
@@ -0,0 +1,168 @@
+// The sample model.  You should build a file
+// very similar to this for when you make your model.
+#include "modelerview.h"
+#include "modelerapp.h"
+#include "modelerdraw.h"
+#include <FL/gl.h>
+
+#include "modelerglobals.h"
+
+// To make a SampleModel, we inherit off of ModelerView
+class SampleModel : public ModelerView 
+{
+public:
+    SampleModel(int x, int y, int w, int h, char *label) 
+        : ModelerView(x,y,w,h,label) { }
+
+    virtual void draw();
+};
+
+// We need to make a creator function, mostly because of
+// nasty API stuff that we'd rather stay away from.
+ModelerView* createSampleModel(int x, int y, int w, int h, char *label)
+{ 
+    return new SampleModel(x,y,w,h,label); 
+}
+
+// We are going to override (is that the right word?) the draw()
+// method of ModelerView to draw out SampleModel
+void SampleModel::draw()
+{
+    // This call takes care of a lot of the nasty projection 
+    // matrix stuff.  Unless you want to fudge directly with the 
+	// projection matrix, don't bother with this ...
+    ModelerView::draw();
+
+	// draw the floor
+	setAmbientColor(.1f,.1f,.1f);
+	setDiffuseColor(COLOR_RED);
+	glPushMatrix();
+	glTranslated(-5,0,-5);
+	drawBox(10,0.01f,10);
+	glPopMatrix();
+
+
+
+	setAmbientColor(.1f, .1f, .1f);
+	setDiffuseColor(COLOR_GREEN);
+	glPushMatrix();
+	glTranslated(VAL(XPOS), VAL(YPOS),VAL(ZPOS));
+	//draw the troso
+		glPushMatrix();
+		glTranslated(-1.1, 3.4, -0.8);
+		drawBox(2.2, 3.1, 1.6);
+		glTranslated(1.05, -0.2, 0.7);
+		drawBox(0.2,0.2,0.2);
+		glPopMatrix();
+
+		//draw the beam
+		glPushMatrix();
+		glTranslated(0, 3.0, 0);
+		glRotated(VAL(ROTATE), 0.0, 1.0, 0.0);
+		glRotated(90, 0.0, 1.0, 0.0);
+		glTranslated(0, 0, -1.25);		
+		drawCylinder(2.4, 0.25, 0.25);
+			//draw the left upper arm
+			glPushMatrix();
+			drawCylinder(0.2, 0.3, 0.3);
+			glTranslated(0.0, 0.0, 0.1);
+			glRotated(-90, 0.0, 1.0, 0.0);
+			glRotated(115, 1.0, 0.0, 0.0);				
+			drawCylinder(1.0,0.2,0.3);
+			glTranslated(0, 0, 1.2);
+
+				//draw the left lower arm
+				glPushMatrix();
+				drawSphere(0.25);
+				glRotated(-65, 1.0, 0.0, 0.0);
+				drawCylinder(1.65, 0.2, 0.2);
+				glTranslated(0.0,0.0,1.65);
+
+					//draw the right hand 
+					glPushMatrix();
+					drawSphere(0.2);
+					glTranslated(-0.25, 0, 0);
+					drawBox(0.1, 0.2, 0.4);
+					glTranslated(0.4, 0, 0);
+					drawBox(0.1, 0.2, 0.4);
+					glPopMatrix();
+				glPopMatrix();				
+			glPopMatrix();
+
+		glTranslated(0, 0, 2.4);
+			//draw the right upper arm
+			glPushMatrix();
+			drawCylinder(0.2, 0.3, 0.3);
+			glTranslated(0.0, 0.0, 0.1);
+			glRotated(-90, 0.0, 1.0, 0.0);
+			glRotated(115, 1.0, 0.0, 0.0);
+			drawCylinder(1.0, 0.2, 0.3);
+			glTranslated(0, 0, 1.2);
+
+				//draw the right lower arm
+				glPushMatrix();
+				drawSphere(0.25);
+				glRotated(-65, 1.0, 0.0, 0.0);
+				drawCylinder(1.65, 0.2, 0.2);
+				glTranslated(0.0, 0.0, 1.65);
+
+					//draw the right hand 
+					glPushMatrix();
+					drawSphere(0.2);
+					glTranslated(-0.25, 0, 0);
+					drawBox(0.1, 0.2, 0.4);
+					glTranslated(0.4, 0, 0);
+					drawBox(0.1, 0.2, 0.4);
+					glPopMatrix();
+				glPopMatrix();
+			glPopMatrix();
+
+		glPopMatrix();
+	glPopMatrix();
+
+
+	/*// draw the sample model
+	setAmbientColor(.1f,.1f,.1f);
+	setDiffuseColor(COLOR_GREEN);
+	glPushMatrix();
+	glTranslated(VAL(XPOS), VAL(YPOS), VAL(ZPOS));
+
+		glPushMatrix();
+		glTranslated(-1.5, 0, -2);
+		glScaled(3, 1, 4);
+		drawBox(1,1,1);
+		glPopMatrix();
+
+		// draw cannon
+		glPushMatrix();
+		glRotated(VAL(ROTATE), 0.0, 1.0, 0.0);
+		glRotated(-90, 1.0, 0.0, 0.0);
+		drawCylinder(VAL(HEIGHT), 0.1, 0.1);
+
+		glTranslated(0.0, 0.0, VAL(HEIGHT));
+		drawCylinder(1, 1.0, 0.9);
+
+		glTranslated(0.0, 0.0, 0.5);
+		glRotated(90, 1.0, 0.0, 0.0);
+		drawCylinder(4, 0.1, 0.2);
+		glPopMatrix();
+
+	glPopMatrix();
+	*/
+}
+
+int main()
+{
+	// Initialize the controls
+	// Constructor is ModelerControl(name, minimumvalue, maximumvalue, 
+	// stepsize, defaultvalue)
+    ModelerControl controls[NUMCONTROLS];
+    controls[XPOS] = ModelerControl("X Position", -5, 5, 0.1f, 0);
+    controls[YPOS] = ModelerControl("Y Position", 0, 5, 0.1f, 0);
+    controls[ZPOS] = ModelerControl("Z Position", -5, 5, 0.1f, 0);
+    controls[HEIGHT] = ModelerControl("Height", 1, 2.5, 0.1f, 1);
+	controls[ROTATE] = ModelerControl("Rotate", -135, 135, 1, 0);
+
+    ModelerApplication::Instance()->Init(&createSampleModel, controls, NUMCONTROLS);
+    return ModelerApplication::Instance()->Run();
+}
